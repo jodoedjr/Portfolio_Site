28f64c00f0df65163ee9895f209c028b0a4194c2
John Edwards <jodoedjr@gmail.com>
2020-10-30 21:40:51 -0400

    moving

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={["/", "/home", "/index"]} component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NoMatch} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,29 +1,8 @@
 import React from "react";
-import "materialize-css";
-import { Button, Card, CardTitle, Icon, Row, Col } from "react-materialize";
 
 const Projects = () => {
     return (
-<Row>
-  <Col
-    m={6}
-    s={12}
-  >
-    <Card
-      closeIcon={<Icon>close</Icon>}
-      header={<CardTitle image="https://materializecss.com/images/sample-1.jpg" reveal waves="light"/>}
-      reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
-      revealIcon={<Icon>more_vert</Icon>}
-      title="Card Title"
-    >
-      <p>
-        <a href="#">
-          This is a link
-        </a>
-      </p>
-    </Card>
-  </Col>
-</Row>
+      <h1>PROJECTS</h1>
     )
 }
 
