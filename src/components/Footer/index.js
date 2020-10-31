8687ae817019633896d85adb85d1237444553ee5
John Edwards <jodoedjr@gmail.com>
2020-10-31 17:53:22 -0400

    a

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,42 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { FaGithub } from 'react-icons/fa';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 
 const Footer = (props) => {
     return (
         <footer className="bg-dark">
-            
-                <ul className="list-unstyled list-inline text-center">
-                    <li className="list-inline-item">
-                        <a className="btn-floating btn-fb mx-1">
-                        <FaGithub style={{color:"white",height:"100px",width:"100px"}}></FaGithub>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a className="btn-floating btn-tw mx-1">
-                            <i className="fab fa-twitter"> </i>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a className="btn-floating btn-gplus mx-1">
-                            <i className="fab fa-google-plus-g"> </i>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a className="btn-floating btn-li mx-1">
-                            <i className="fab fa-linkedin-in"> </i>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a className="btn-floating btn-dribbble mx-1">
-                            <i className="fab fa-dribbble"> </i>
-                        </a>
-                    </li>
-                </ul>
-            <div className="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+            <FaGithub />
+            <FaLinkedin />
+            <div className="footer-copyright text-center py-3">© 2020 Copyright John Edwards
             </div>
         </footer>
     )
